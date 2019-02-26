84fe5b04ef75b63ceadf9efcbde175a7fa38d617
Nirav_Sangani <nirav.sangani@sufulamtech.com>
2019-02-26 17:37:21 +0530

    Add Date Formatter in Lib

--- app/src/main/java/com/dateformater/MainActivity.java
+++ app/src/main/java/com/dateformater/MainActivity.java
@@ -2,12 +2,45 @@ package com.dateformater;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.dateformatter.FormatConversation;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static String TAG = MainActivity.class.getSimpleName();
+
+    String strDate = "26-2-2019";
+    String strCreatedDate = "24-2-2018";
+    String strCurrentDate = "26-2-2019";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+     /*   *//**Convert String to String Date Format : strDateFormateConversion *//*
+        Log.e(TAG, "onCreate: strDateFormateConversion : " + strDate + " ::-> " + FormatConversation.strDateFormateConversion(strDate, FormatConversation.dd_MM_yyyy, FormatConversation.yyyy_MM_dd_HH_mm_ss));
+
+        *//**Convert Date to String with desired format : dateToStringConversion *//*
+        Log.e(TAG, "onCreate: Convert Date to String with desired format : " + strDate + " ::-> "
+                + FormatConversation.dateToStringConversion(FormatConversation.strDateToDateConversion(strDate, FormatConversation.dd_MM_yyyy),
+                FormatConversation.dd_MM_yyyy));
+
+        *//** Get Time Ago From Two Dates : getTimeAgo *//*
+        Log.e(TAG, "onCreate: getTimeAgo : " + strCreatedDate + " ::-> "
+                + FormatConversation.getTimeAgo(FormatConversation.strDateToDateConversion(strCreatedDate, FormatConversation.dd_MM_yyyy), FormatConversation.strDateToDateConversion(strCurrentDate, FormatConversation.dd_MM_yyyy)));
+
+        *//** Get difference between two date in days, hours, minutes and seconds : getDateDifferenceInX *//*
+        Log.e(TAG, "onCreate: getDateDifferenceInX : " + strCreatedDate + " ::-> "
+                + FormatConversation.getDateDifferenceInX(FormatConversation.strDateToDateConversion(strCurrentDate, FormatConversation.dd_MM_yyyy), FormatConversation.strDateToDateConversion(strCreatedDate, FormatConversation.dd_MM_yyyy), FormatConversation.RemainingType.DAYS));
+
+        *//** Get Duration from given seconds in HH:mm:ss format: getDurationFromSeconds *//*
+        Log.e(TAG, "onCreate: getDurationFromSeconds : 12500 ::-> "
+                + FormatConversation.getDurationFromSeconds(12500));*/
+
+
+        //  Log.e(TAG, "onCreate: " + FormatConversation.getDateBeforeAfterXDays(2));
     }
 }
--- dateformatter/src/main/java/com/dateformatter/FormatConversation.java
+++ dateformatter/src/main/java/com/dateformatter/FormatConversation.java
@@ -12,53 +12,17 @@ import java.util.TimeZone;
 public class FormatConversation {
 
 
-    public static final String yyyy_MM_dd_HH_mm_ss = "yyyy-MM-dd HH:mm:ss";
-    public static final String yyyy_MM_dd_T_HH_mm_ss_SSS_Z = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
-    public static final String dd_MMM_yyyy_hh_mm_a = "dd MMM yyyy hh:mm a";
-    public static final String dd_MMM_yyyy = "dd MMM yyyy";
-    public static final String YYYY_MM_DD = "yyyy-MM-dd";
-    public static final String MMMM_DD_YYYY_HH_MM_A = "MMMM dd,yyyy hh.mm a";
-
-
-    public static String timestampToDateStrConversion(long timestamp, String DateFormatString) {
-        Date convertedDate = getDateFromTimestamp(timestamp);
-        return dateToStringConversion(convertedDate, DateFormatString);
-    }
-
     /**
-     * Convert String to Date Object
-     *
-     * @param dateString
-     * @param DateFormatString (gives the format of String in which format you pass the string
-     *                         ex. StringToDateConversion("02/12/2015","dd//MM/yyyy")
-     * @return Date object
-     * <p/>
-     * Date object always gives standard Data Format
-     */
-    public static Date stringToDateConversion(String dateString, String DateFormatString) {
-        Date convertedDate = new Date();
-        try {
-            SimpleDateFormat dateFormat = new SimpleDateFormat(DateFormatString);
-            convertedDate = dateFormat.parse(dateString);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        return convertedDate;
-    }
-
-    /**
-     * Convert String to Date Object
+     * Convert String to String Date Format
      *
      * @param dateString
      * @param currentFormat  (gives the format of String in which format you pass the string
-     *                       ex. StringToDateConversion("02/12/2015","dd//MM/yyyy")
+     *                       ex. strDateFormateConversion("02/12/2015","dd/MM/yyyy")
      * @param requiredFormat (In witch format you want string)
-     * @return Date object
-     * <p/>
-     * Date object always gives standard Data Format
+     * @return String
+     * In Return retrive your require date format in string
      */
-    public static String stringToStringConversion(String dateString, String currentFormat, String requiredFormat) {
+    public static String strDateFormateConversion(String dateString, String currentFormat, String requiredFormat) {
         String convertedDateStr = "";
 
         if (dateString != null && !dateString.isEmpty() && currentFormat != null && !currentFormat.isEmpty() && requiredFormat != null && !requiredFormat.isEmpty()) {
@@ -74,22 +38,6 @@ public class FormatConversation {
         return convertedDateStr;
     }
 
-    public static String oneStringToOtherString(String currentFormat, String newFormat, String inputString) {
-
-        String reformattedStr = "";
-
-        SimpleDateFormat fromUser = new SimpleDateFormat(currentFormat, Locale.ENGLISH);
-        SimpleDateFormat myFormat = new SimpleDateFormat(newFormat, Locale.ENGLISH);
-
-        try {
-
-            reformattedStr = myFormat.format(fromUser.parse(inputString));
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        return reformattedStr;
-    }
 
     /**
      * Convert Date to String with desired format
@@ -98,7 +46,6 @@ public class FormatConversation {
      * @param DateFormatString (In witch format you want string)
      * @return
      */
-
     public static String dateToStringConversion(Date date, String DateFormatString) {
         String dateString = "";
         try {
@@ -107,11 +54,80 @@ public class FormatConversation {
 
             dateString = getDateInEnglish(dateString, DateFormatString);
         } catch (Exception e) {
-
             e.printStackTrace();
         }
 
         return dateString;
+    }
+
+    /**
+     * Convert String to Date Object
+     *
+     * @param dateString
+     * @param DateFormatString (gives the format of String in which format you pass the string
+     *                         ex. strDateToDateConversion("02/12/2015","dd/MM/yyyy")
+     * @return Date object
+     * <p/>
+     * Date object always gives standard Data Format
+     */
+    public static Date strDateToDateConversion(String dateString, String DateFormatString) {
+        Date convertedDate = new Date();
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat(DateFormatString);
+            convertedDate = dateFormat.parse(dateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return convertedDate;
+    }
+
+    /**
+     * Get Time Ago From Two Dates
+     *
+     * @param createdDte (gives the Created Date in Date format
+     * @param currentDte (gives the Current Date in Date format
+     * @return String
+     * <p/>
+     * String object always gives Year,Month,Day,Hours,Minutes,Seconds
+     * ex. 1 year
+     * ex. 2 years
+     */
+    public static String getTimeAgo(Date createdDte, Date currentDte) {
+
+        if (createdDte != null) {
+            long diff = currentDte.getTime() - createdDte.getTime();
+            long seconds = diff / 1000;
+            long minutes = seconds / 60;
+            long hours = minutes / 60;
+            long days = hours / 24;
+            long months = days / 30;
+            long years = months / 12;
+            if (createdDte.before(currentDte)) {
+
+                if (years > 0) {
+                    return "" + years + ((years > 1) ? " years" : " year");
+
+                } else if (months > 0) {
+                    return "" + months + ((months > 1) ? " months" : " month");
+
+                } else if (days > 0) {
+                    return "" + days + ((days > 1) ? " days" : " day");
+
+                } else if (hours > 0) {
+                    return "" + hours + ((hours > 1) ? " hours" : " hour");
+
+                } else if (minutes > 0) {
+                    return "" + minutes + ((minutes > 1) ? " minutes" : " minute");
+
+                } else if (seconds > 0) {
+                    return "" + seconds + ((seconds > 1) ? " seconds" : " second");
+                } else {
+                    return "";
+                }
+            }
+        }
+        return "";
     }
 
     public enum RemainingType {
@@ -129,7 +145,6 @@ public class FormatConversation {
      * @param remainingType - specify type(days,hours, minutes and seconds)to find difference between two dates.
      * @return
      */
-
     public static long getDateDifferenceInX(Date startDate, Date endDate, RemainingType remainingType) {
 
         long remaining = 0;
@@ -149,6 +164,31 @@ public class FormatConversation {
     }
 
     /**
+     * Get the Date before and After X Days
+     *
+     * @param days
+     * @return
+     */
+    public static Date getDateBeforeAfterXDays(int days) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_MONTH, -days);
+
+        return calendar.getTime();
+    }
+
+    /**
+     * Get String Date from given Timestamp
+     *
+     * @param timestamp        - provide timestamp for gettimg string date of given timestamp
+     * @param DateFormatString - provide string date format for gettimg string date of given timestamp
+     * @return
+     */
+    public static String timestampToStrDateConversion(long timestamp, String DateFormatString) {
+        Date convertedDate = getDateFromTimestamp(timestamp);
+        return dateToStringConversion(convertedDate, DateFormatString);
+    }
+
+    /**
      * Get Date from given Timestamp
      *
      * @param timestamp - provide timestamp for gettimg date of given timestamp
@@ -156,12 +196,15 @@ public class FormatConversation {
      */
 
     public static Date getDateFromTimestamp(long timestamp) {
-        Calendar calendar = Calendar.getInstance();
-        TimeZone tz = TimeZone.getDefault();
-        calendar.setTimeZone(tz);
-        calendar.setTimeInMillis(timestamp);
+        if (timestamp > 0) {
+            Calendar calendar = Calendar.getInstance();
+            TimeZone tz = TimeZone.getDefault();
+            calendar.setTimeZone(tz);
+            calendar.setTimeInMillis(timestamp);
 
-        return calendar.getTime();
+            return calendar.getTime();
+        }
+        return null;
     }
 
     /**
@@ -170,7 +213,6 @@ public class FormatConversation {
      * @param date - provide date for getting timestamp of given date
      * @return
      */
-
     public static long getTimestampFromDate(Date date) {
         Calendar cal = Calendar.getInstance();
         cal.setTime(date);
@@ -179,32 +221,23 @@ public class FormatConversation {
     }
 
     /**
-     * Pass the Date and with desired format. It return string with given format
+     * Get Duration from given seconds in HH:mm:ss format
      *
-     * @param date             -
-     * @param dateFormatString - in which format you want to get date.
+     * @param seconds - provide seconds for getting duration of given seconds
      * @return
      */
+    public static String getDurationFromSeconds(int seconds) {
 
-    public static String changeDateFormat(Date date, String dateFormatString) {
-        SimpleDateFormat sdf = new SimpleDateFormat(dateFormatString);
+        if (seconds > 0) {
+            int hours = seconds / 3600;
+            int minutes = (seconds % 3600) / 60;
+            seconds = seconds % 60;
 
-        return sdf.format(date);
+            return "" + twoDigitString(hours) + ":" + twoDigitString(minutes) + ":" + twoDigitString(seconds);
+        }
+        return "";
     }
 
-    /**
-     * Get the Date before and After X Days
-     *
-     * @param days
-     * @return
-     */
-
-    public static Date getDateBeforeAfterXDays(int days) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.DAY_OF_MONTH, -days);
-
-        return calendar.getTime();
-    }
 
     public static String getDateInEnglish(String inputDate, String dateFormat) {
 
@@ -222,4 +255,16 @@ public class FormatConversation {
         return finalDate;
     }
 
+    public static String twoDigitString(int number) {
+
+        if (number == 0) {
+            return "00";
+        }
+
+        if (number / 10 == 0) {
+            return "0" + number;
+        }
+
+        return String.valueOf(number);
+    }
 }
